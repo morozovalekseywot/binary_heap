34c80a4d82418d000070c19361fb795bfd2a0767
morozovalekseywot <morozovalekseywot@gmail.com>
2021-05-28 21:32:23 +0300

    merge

--- main.cpp
+++ main.cpp
@@ -4,60 +4,10 @@
 #include "tests.hpp"
 int main()
 {
-    /*
-    vector<int> vec = {2, 4, 3, 7, 6, 5, 3, 2};
-    Heap_Array<int> heap(vec);
-    for (int i = 0; i < heap.data.size(); i++)
-        cout << heap.data[i] << " ";
-    cout << "\n";
-    //cout << heap.search(1);
-    heap.insert(10);
-    heap.insert(8);
-    heap.insert(9);
-    heap.insert(16);
-    heap.insert(11);
-    heap.insert(12);
-    par_node(heap);
-
-    //heap.remove(10);
-    auto b = heap.sub_heap(10);
-    par_node(b);
-    Heap_Array<int> sub({10, 8, 9, 7});
-    par_node(sub);
-    //sub.printNLR2(0,cout);
-    cout << "\n";
-    Set_Array<int> Set_b = Set_Array<int>({1, 8, 10, 2});
-    Set_Array<int> Set_a = Set_Array<int>({11, 4, 8, 1});
-    cout<<"Set_Array a: "<<Set_a<<"\n";
-    cout<<"Set_Array b: "<<Set_b<<"\n";
-    //cout<<heap.size()<<" "<<Set_a.size();
-    Set_Array<int> Set_sum = Set_a + Set_b;
-    cout <<"Size of Set_sum: " <<Set_sum.size() << ", Set_Array: " << Set_sum<<"\n";
-    Set_Array<int> Set_diff = Set_a - Set_b;
-    cout <<"Size of Set_diff: " <<Set_diff.size() << ", Set_Array: " << Set_diff<<"\n";
-    Set_Array<int> Set_in = Set_a * Set_b;
-    cout <<"Size of Set_in: " <<Set_in.size() << ", Set_Array: " << Set_in<<"\n";
-    Set_Array<int> Set_str = Set_Array<int>("-12 7328 92 2 -749 3286 -2398 234023",1);
-    cout<<Set_str<<"\n";
-     */
-    /*
-    Set_vec<int> vec1 = Set_vec<int>({1, 2, 3, 4});
-    Set_vec<int> vec2 = Set_vec<int>({6, 2, 3, 9});
-    cout << vec1 + vec2 << "\n" << vec1 - vec2 << "\n" << vec1 * vec2 << "\n";
-    vec1.insert(6);
-    vec1.insert(-2);
-    vec1.insert(5);
-    vec1.insert(3);
-    cout << vec1 << "\n";
-    vec1.remove(-2);
-    vec1.remove(5);
-    cout<<vec1.include(vec2)<<" ";
-    vec2.remove(9);
-    cout << vec1 << " "<<vec1.include(vec2)<<" "<<vec1.search(5);
-     */
     ofstream file("Compare.txt", ios_base::trunc);
     file << "Function name\ntime array time Linked_List\n";
     file.close();
     write_results(test_create(1e3),"Compare.txt","Test_Create");
     write_results(test_insert(1e3),"Compare.txt","Test_Insert");
+    write_results(test_remove(1e3),"Compare.txt","Test_Remove");
 }
--- tests.hpp
+++ tests.hpp
@@ -24,17 +24,17 @@ pair<int, int> test_create(const int n)
     }
 
     auto startTime = chrono::steady_clock::now();
-    Set_Node<int> Set_Node(values);
+    Set_Node<int> set_node(values);
     auto endTime = chrono::steady_clock::now();
     auto node_time = chrono::duration_cast<chrono::microseconds>(endTime - startTime).count();
 
     startTime = chrono::steady_clock::now();
-    Set_Array<int> Set_Array(values);
+    Set_Array<int> set_array(values);
     endTime = chrono::steady_clock::now();
     auto array_time = chrono::duration_cast<chrono::microseconds>(endTime - startTime).count();
 
-    assert(Set_Node == target);
-    assert(Set_Array == target);
+    assert(set_node == target);
+    assert(set_array == target);
 
     return {node_time, array_time};
 }
@@ -69,6 +69,36 @@ pair<int, int> test_insert(const int n)
 
     assert(set_node == target);
     assert(set_array == target);
+
+    return {node_time, array_time};
+}
+
+pair<int, int> test_remove(const int n)
+{
+    default_random_engine generator;
+    uniform_int_distribution<int> distribution(-INT32_MAX+3,INT32_MAX-3);
+    vector<int> values(n);
+    for (int i = 0; i < n; i++)
+    {
+        int value = distribution(generator);
+        values[i] = value;
+    }
+    Set_Node<int> set_node(values);
+    Set_Array<int> set_array(values);
+    auto startTime = chrono::steady_clock::now();
+    for (int i = 0; i < n; i++)
+        set_node.remove(values[i]);
+    auto endTime = chrono::steady_clock::now();
+    int node_time = chrono::duration_cast<chrono::microseconds>(endTime - startTime).count();
+
+    startTime = chrono::steady_clock::now();
+    for (int i = 0; i < n; i++)
+        set_array.remove(values[i]);
+    endTime = chrono::steady_clock::now();
+    int array_time = chrono::duration_cast<chrono::microseconds>(endTime - startTime).count();
+
+    assert(set_node.data.root == nullptr);
+    assert(set_array.size() == 0);
 
     return {node_time, array_time};
 }
